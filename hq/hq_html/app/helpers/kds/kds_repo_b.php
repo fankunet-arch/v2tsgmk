d1f47397aaa3e18164a0807fe652c66c522e43fc
fankunet-arch <fankunet@gmail.com>
2025-11-09 19:04:37 +0100

    Delete hq/hq_html directory

--- hq/hq_html/app/helpers/kds/kds_repo_b.php
+++
@@ -1,3 +0,0 @@
-<?php
-// KDS Repo B — intentionally left blank to avoid duplicate function declarations.
-// Do not add functions here that exist in kds_repo_a.php / kds_repo_c.php / kds_repo_fix.php.
